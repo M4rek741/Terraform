edd429a923ffb653860a05623c777946d4d658a0
Marek <spinaxcz@gmail.com>
2026-02-25 08:08:04 +0000

    Added Providers block, moved terraform.tf file there

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@
 #Terraform Resource Block is used to manage ANY infrastructure (EC2/S3_Bucket/Whatever) we want to debloy on AWS.
 
 # resource <BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
- # Block body
+# Block body
 # <IDENTIFIER> = <EXPRESSION> # Argument
 # }
 
@@ -29,64 +29,64 @@
 
 # Configure the AWS Provider
 provider "aws" {
- #  access_key = "_NULL_"
- #  secret_key = "_NULL_"
-   region = "us-east-1"
-   shared_credentials_files = ["/root/TERRAFORM/.aws/credentials"]
- }
+  #  access_key = "_NULL_"
+  #  secret_key = "_NULL_"
+  region                   = "us-east-1"
+  shared_credentials_files = ["/root/TERRAFORM/.aws/credentials"]
+}
 
 # Create an EC2 instance called “web”.
 resource "aws_instance" "web" {
-    ami		 	        = "ami-0c1fe732b5494dc14"
-    instance_type		= "t3.micro"
-    subnet_id			= "subnet-0561a24c2667c679b"
-    vpc_security_group_ids	= ["sg-04fc0cf9ea5598a43"]
+  ami                    = "ami-0c1fe732b5494dc14"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0561a24c2667c679b"
+  vpc_security_group_ids = ["sg-04fc0cf9ea5598a43"]
 
-    tags = {
-        Terraform = "True"
-        Purpose = "Hashi Corp configuration language Lab"
-    }
+  tags = {
+    Terraform = "True"
+    Purpose   = "Hashi Corp configuration language Lab"
+  }
 }
 
 # Add a new resource - "VPC"
 resource "aws_vpc" "vpc" {
-   cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-   tags = {
-     Name = var.vpc_name
-     Terraform = "true"
-     Region = data.aws_region.current
-   }
+  tags = {
+    Name      = var.vpc_name
+    Terraform = "true"
+    Region    = data.aws_region.current.name
+  }
 }
 
 data "aws_region" "current" {}
 
 # Add a new resource - "Internet gateway"
 resource "aws_internet_gateway" "internet_gateway" {
-   vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 }
 
 # Add a new resource - "Route Table"
 resource "aws_route_table" "public_route_table" {
-   vpc_id = aws_vpc.vpc.id
-   
-   route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.internet_gateway.id
-   }
-   tags = {
-     Name = "demo_igw"
-     Terraform = "true"
-   }
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "demo_igw"
+    Terraform = "true"
+  }
 }
 
 # Add a new resource - "S3 bucket"
- resource "aws_s3_bucket" "my-new-S3-bucket" {
+resource "aws_s3_bucket" "my-new-S3-bucket" {
   bucket = "my-new-Test-bucket-Marek"
-   tags = {
-     Name    = "My S3 Bucket"
-     Purpose = "Intro to Resource Blocks Lab"
-	}
+  tags = {
+    Name    = "My S3 Bucket"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
 }
 
 # Add a new resource (an ACL) - "S3 bucket ACL"
@@ -97,44 +97,14 @@ resource "aws_route_table" "public_route_table" {
 
 
 # Add a new resource block WITH variables - insted of hard coded values.
-resource aws_subnet "terraform-subnet"{
-   vpc_id			= aws_vpc.vpc.id
-   cidr_block			= var.variable_sub_cidr
-   availability_zone		= var.variable_sub_az
-   map_public_ip_on_launch	= var.variable_sub_auto_ip	
+resource "aws_subnet" "terraform-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variable_sub_cidr
+  availability_zone       = var.variable_sub_az
+  map_public_ip_on_launch = var.variable_sub_auto_ip
 
-   tags = {
-     Name	= "sub-variables-${var.variable_sub_az}"
-     Terraform	= "true"
+  tags = {
+    Name      = "sub-variables-${var.variable_sub_az}"
+    Terraform = "true"
   }
 }
-
-
-
-
-
-
-
-
-
-
-#TRASH:
-#resource "aws_s3_bucket" "my_new_S3_bucket" {
-#    bucket = "my-new-tf-test-bucket-marek"
-#    acl    = "private"
-
-#    tags = {
-#        Name = "My S3 Bucket"
-#        Purpose = "Intor to Resource Block Lab"
-#    }
-#}
-
-#resource "aws_s3_bucket_acl" "my_new_bucket_acl"{
-#    bucket = aws_s3_bucket.my_new_S3_bucket.id
-#    acl    = "private" 
-#}
-
-#Task: Add new provider.
-#resource "random_id" "randomness_name_just_for_terminal"{
-#    byte_length = 16
-#}
--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "variable_sub_cidr" {
-  description	= "CIDR block for the Variable Subnet"
-  type		= string
+  description = "CIDR block for the Variable Subnet"
+  type        = string
 }
 
 variable "variable_sub_az" {
-  description	= "Availability Zone used Variables Subnet"
-  type		= string
+  description = "Availability Zone used Variables Subnet"
+  type        = string
 }
 
 variable "variable_sub_auto_ip" {
-  description	= "Set Automatic IP Assignement for Variables Subnet"
-  type		= bool
+  description = "Set Automatic IP Assignement for Variables Subnet"
+  type        = bool
 }
 
 variable "vpc_name" {
